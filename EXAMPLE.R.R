03bb7f8a603cdc8cebb297c65479d6b3587a76be
vivek8090 <62151546+vivek8090@users.noreply.github.com>
2020-08-27 19:56:50 +0530

    Add files via upload

---
+++ EXAMPLE.R.R
@@ -0,0 +1,27 @@
+1+1
+2+3*4
+#create a vector
+x<-c(12,20,15,100)
+my_vect<-c(12,20,15,100)
+vect_x<-c(2,0,0,4)
+vect_y<-c(1,9,9,9)
+# add the vectors
+vect_x+vect_y
+#mul vect_x with some scalar
+vect_x*5
+vect_y*3
+#acessing the elements of the vector
+vect_x[1]
+vect_x[4]
+vect_x[2]
+vect_x[2]<-2
+vect_x
+char_x<- c("a","b","c")
+vect_char <- c('R','python','java',10,100.5)
+x<-58
+y<-20
+z<-100
+x*y*z
+sqrt(10)
+exp(1)
+(6*3)+5/2
